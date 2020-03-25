111b8a1ba65036c0e42e2d69dcefaebf96f17829
f.berchtold <f.berchtold@samsung.com>
2020-03-25 17:11:27 +0100

    done

--- my_project/my_project.hpp
+++ my_project/my_project.hpp
@@ -1,4 +1,40 @@
+#include "string"
+using namespace std;
 
-int CalculateEverything(){
-    return 42;
+std::string EvaluateNumber(int number, string base, string middle, string next) {
+  switch (number % 10) {
+    case 1:
+      return base;
+    case 2:
+      return base + base;
+    case 3:
+      return base + base + base;
+    case 4:
+      return base + middle;
+    case 5:
+      return middle;
+    case 6:
+      return middle + base;
+    case 7:
+      return middle + base + base;
+    case 8:
+      return middle + base + base + base;
+    case 9:
+      return base + next;
+    default:
+      return "";
+  }
+}
+
+std::string GetRoman(int number) {
+  std::string result = "";
+  int number_of_M = number / 1000;
+  for (int i = 0; i < number_of_M; ++i) {
+    result += "M";
+  }
+  result += EvaluateNumber(number / 100, "C", "D", "M");
+  result += EvaluateNumber(number / 10, "X", "L", "C");
+  result += EvaluateNumber(number / 1, "I", "V", "X");
+
+  return result;
 }
--- my_project/my_project_spec.cpp
+++ my_project/my_project_spec.cpp
@@ -2,6 +2,84 @@
 
 #include "my_project.hpp"
 
-TEST(First, test) {
-  EXPECT_EQ(42, CalculateEverything());
+TEST(Roman, first) {
+  EXPECT_EQ("I", GetRoman(1));
+}
+
+TEST(Roman, second) {
+  EXPECT_EQ("II", GetRoman(2));
+}
+
+TEST(Roman, third) {
+  EXPECT_EQ("III", GetRoman(3));
+}
+
+TEST(Roman, fourth) {
+  EXPECT_EQ("IV", GetRoman(4));
+}
+
+TEST(Roman, fifth) {
+  EXPECT_EQ("V", GetRoman(5));
+}
+
+TEST(Roman, sixth) {
+  EXPECT_EQ("VI", GetRoman(6));
+}
+
+TEST(Roman, seventh) {
+  EXPECT_EQ("VII", GetRoman(7));
+}
+TEST(Roman, eight) {
+  EXPECT_EQ("VIII", GetRoman(8));
+}
+TEST(Roman, ninth) {
+  EXPECT_EQ("IX", GetRoman(9));
+}
+
+TEST(Roman, tenth) {
+  EXPECT_EQ("X", GetRoman(10));
+}
+
+TEST(Roman, twenty) {
+  EXPECT_EQ("XX", GetRoman(20));
+}
+
+TEST(Roman, fifty) {
+  EXPECT_EQ("L", GetRoman(50));
+}
+
+TEST(Roman, ninety) {
+  EXPECT_EQ("XC", GetRoman(90));
+}
+
+TEST(Roman, fourtyseven) {
+  EXPECT_EQ("XLVII", GetRoman(47));
+}
+
+TEST(Roman, ninetynine) {
+  EXPECT_EQ("XCIX", GetRoman(99));
+}
+
+TEST(Roman, hundred) {
+  EXPECT_EQ("C", GetRoman(100));
+}
+
+TEST(Roman, hundredeleven) {
+  EXPECT_EQ("CXI", GetRoman(111));
+}
+
+TEST(Roman, fourhundredfive) {
+  EXPECT_EQ("CDV", GetRoman(405));
+}
+
+TEST(Roman, ninehundredninetynine) {
+  EXPECT_EQ("CMXCIX", GetRoman(999));
+}
+
+TEST(Roman, _3999) {
+  EXPECT_EQ("MMMCMXCIX", GetRoman(3999));
+}
+
+TEST(Roman, fourthousands) {
+  EXPECT_EQ("MMMM", GetRoman(4000));
 }
