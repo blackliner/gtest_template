9ab2a933d5e24ad9d3c6fe1c98b608cab6dd9446
f.berchtold <f.berchtold@samsung.com>
2020-04-08 16:54:06 +0200

    Done, Fadoua, Pavel and Florent :-)

--- interp2d/interp2d.hpp
+++ interp2d/interp2d.hpp
@@ -1,4 +1,48 @@
+#include <cmath>
+#include <stdexcept>
+#include <utility>
+#include <vector>
 
-int CalculateEverything(){
-    return 42;
+using DataGrid = std::vector<std::vector<float>>;
+using LookupPoint = std::pair<float, float>;
+
+float Interpolate2D(const DataGrid& grid, const LookupPoint& lookup_point) {
+  if (grid.empty()) {
+    throw std::invalid_argument("expected non empty grid");
+  }
+
+  const auto x = lookup_point.first;
+  const auto y = lookup_point.second;
+
+  if (x < 0 || y < 0 || (x + 1) > grid.size() || (y + 1) > grid[0].size()) {
+    throw std::invalid_argument("point out of bounds");
+  }
+
+  int floor_x = std::floor(x);
+  int ceil_x = std::ceil(x);
+  int floor_y = std::floor(y);
+  int ceil_y = std::ceil(y);
+
+  auto interpolated_bottom = grid[floor_x][floor_y] + x * (grid[ceil_x][floor_y] - grid[floor_x][floor_y]);
+  auto interpolated_top = grid[floor_x][ceil_y] + x * (grid[ceil_x][ceil_y] - grid[floor_x][ceil_y]);
+
+  auto interpolated = interpolated_bottom + y * (interpolated_top - interpolated_bottom);
+
+  return interpolated;
+}
+
+DataGrid CreateDataGrid(size_t x = 2, size_t y = 2) {
+  DataGrid grid{};
+  grid.resize(x);
+  for (auto& x_axis : grid) {
+    x_axis.resize(y);
+  }
+
+  //    x  y
+  grid[0][0] = 0.0F;  // writes 0.0F to x=0 and y=0
+  grid[1][0] = 1.0F;  // writes 1.0F to x=1 and y=0
+  grid[0][1] = 2.0F;  // writes 2.0F to x=0 and y=1
+  grid[1][1] = 3.0F;  // writes 3.0F to x=1 and y=1
+
+  return grid;
 }
--- interp2d/interp2d_spec.cpp
+++ interp2d/interp2d_spec.cpp
@@ -2,6 +2,97 @@
 
 #include "interp2d.hpp"
 
-TEST(First, test) {
-  EXPECT_EQ(42, CalculateEverything());
+// using DataGrid = std::vector<std::vector<float>>;
+// using LookupPoint = std::pair<float, float>;
+// float Interpolate2D(const DataGrid& data_grid, const LookupPoint& lookup_point);
+
+TEST(Interp2D, emptyGrid) {
+  DataGrid grid{};
+  LookupPoint point{};
+
+  EXPECT_ANY_THROW(Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, nTimesZeroGrid) {
+  DataGrid grid{};
+  grid.resize(3);
+  LookupPoint point{0.F, 0.F};
+
+  EXPECT_ANY_THROW(Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, outOfBoundLookupXNegative) {
+  DataGrid grid = CreateDataGrid();
+
+  LookupPoint point{-1.F, 1.F};
+  EXPECT_ANY_THROW(Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, outOfBoundLookupYNegative) {
+  DataGrid grid = CreateDataGrid();
+
+  LookupPoint point{1.F, -1.F};
+  EXPECT_ANY_THROW(Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, outOfBoundLookupXPositive) {
+  DataGrid grid = CreateDataGrid();
+
+  LookupPoint point{1.2F, 1.F};
+  EXPECT_ANY_THROW(Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, outOfBoundLookupYPositive) {
+  DataGrid grid = CreateDataGrid();
+
+  LookupPoint point{1.0F, 1.3F};
+  EXPECT_ANY_THROW(Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, Boundary00) {
+  DataGrid grid = CreateDataGrid();
+  LookupPoint point{0.0F, 0.0F};
+  EXPECT_EQ(0.0f, Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, Boundary01) {
+  DataGrid grid = CreateDataGrid();
+  LookupPoint point{0.0F, 1.0F};
+  EXPECT_EQ(2.0f, Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, Boundary11) {
+  DataGrid grid = CreateDataGrid();
+  LookupPoint point{1.0F, 1.0F};
+  EXPECT_EQ(3.0f, Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, Boundary10) {
+  DataGrid grid = CreateDataGrid();
+  LookupPoint point{1.0F, 0.0F};
+  EXPECT_EQ(1.0f, Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, Middlex) {
+  DataGrid grid = CreateDataGrid();
+  LookupPoint point{0.5F, 0.0F};
+  EXPECT_EQ(0.5F, Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, Quaterx) {
+  DataGrid grid = CreateDataGrid();
+  LookupPoint point{0.25F, 1.0F};
+  EXPECT_EQ(2.25F, Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, MiddlexMiddley) {
+  DataGrid grid = CreateDataGrid();
+  LookupPoint point{0.5F, 0.5F};
+  EXPECT_EQ(1.5F, Interpolate2D(grid, point));
+}
+
+TEST(Interp2D, FinalBoss) {
+  DataGrid grid = CreateDataGrid();
+  LookupPoint point{0.8F, 0.4F};
+  EXPECT_EQ(1.6F, Interpolate2D(grid, point));
 }
