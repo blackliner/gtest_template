3d1fc9f5f4230d32def1286c76a1d4559ef6bdd1
r.blistein <r.blistein@samsung.com>
2020-05-06 16:54:11 +0200

    basic overflow

--- ring_buffer/ring_buffer.hpp
+++ ring_buffer/ring_buffer.hpp
@@ -13,20 +13,22 @@ class RingBuffer {
   }
 
   std::size_t size() {
-    return head;
+    return end;
   }
 
   void push_back(const T element) {
     m_data[head++] = element;
-    head = head % capacity();
+    end = end % capacity();
   }
 
   T& operator[](const std::size_t index) {
-    return m_data[index];
+    const auto index2 = (end + index) % capacity();
+    return m_data[index2];
   }
 
  private:
-  size_t head{0U};
+  size_t begin{0U};
+  size_t end{0U};
   //   size_t size{0U};
   std::vector<T> m_data;
 };
