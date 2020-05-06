01c08243151402ededd8052453971da9038b6ffb
r.blistein <r.blistein@samsung.com>
2020-05-06 16:27:24 +0200

    initial

--- ring_buffer/ring_buffer.hpp
+++ ring_buffer/ring_buffer.hpp
@@ -1,4 +1,30 @@
 
-int CalculateEverything(){
-    return 42;
-}
+#include <vector>
+
+template <typename T>
+class RingBuffer {
+ public:
+  explicit RingBuffer(const std::size_t capacity) {
+    m_data.reserve(capacity);
+  }
+
+  std::size_t capacity() {
+    return m_data.capacity();
+  }
+
+  std::size_t size() {
+    return m_data.size();
+  }
+
+  void push_back(const T element) {
+    m_data.push_back(element);
+  }
+
+  T& operator[](const std::size_t index) {
+    return m_data[index];
+  }
+
+ private:
+//   size_t end{0U};
+  std::vector<T> m_data;
+};
--- ring_buffer/ring_buffer_spec.cpp
+++ ring_buffer/ring_buffer_spec.cpp
@@ -2,6 +2,38 @@
 
 #include "ring_buffer.hpp"
 
-TEST(First, test) {
-  EXPECT_EQ(42, CalculateEverything());
+TEST(RingBuffer, construction_without_exception) {
+  EXPECT_NO_THROW(RingBuffer<int> ring_buffer{3U});
 }
+
+TEST(RingBuffer, check_capacity) {
+  RingBuffer<int> ring_buffer{3U};
+  EXPECT_EQ(3U, ring_buffer.capacity());
+}
+
+TEST(RingBuffer, check_size) {
+  RingBuffer<int> ring_buffer{3U};
+  EXPECT_EQ(0U, ring_buffer.size());
+}
+
+TEST(RingBuffer, check_push_back) {
+  RingBuffer<int> ring_buffer{3U};
+  ring_buffer.push_back(25);
+  EXPECT_EQ(1U, ring_buffer.size());
+}
+
+TEST(RingBuffer, check_access_operator) {
+  RingBuffer<int> ring_buffer{3U};
+  ring_buffer.push_back(0);
+  ring_buffer.push_back(25);
+  EXPECT_EQ(25, ring_buffer[1U]);
+}
+
+// TEST(RingBuffer, check_overflow) {
+//   RingBuffer<int> ring_buffer{3U};
+//   ring_buffer.push_back(0);
+//   ring_buffer.push_back(1);
+//   ring_buffer.push_back(2);
+//   ring_buffer.push_back(3);
+//   EXPECT_EQ(3, ring_buffer[0U]);
+// }
