01be723f4c827c5e61e70566ade4986d795f55b5
c.bachhuber <c.bachhuber@samsung.com>
2020-05-06 17:30:45 +0200

    SDD session 3

--- ring_buffer/ring_buffer.hpp
+++ ring_buffer/ring_buffer.hpp
@@ -1,4 +1,67 @@
+#include <cstdlib>
+#include <stdexcept>
+#include <vector>
 
-int CalculateEverything(){
-    return 42;
-}
+template <typename T>
+class RingBuffer {
+ public:
+  explicit RingBuffer(const std::size_t capacity) {
+    m_values.reserve(capacity);
+  }
+
+  std::size_t capacity() {
+    return m_values.capacity();
+  }
+
+  std::size_t size() {
+    return m_size;
+  }
+
+  void push_back(const T& element) {
+    ++m_size;
+    m_values.push_back(element);
+  }
+
+  T& back() {
+    return m_values.back();
+  }
+
+  T& front() {
+    return m_values[m_start_index];
+  }
+
+  T& operator[](const std::size_t index) {
+    return m_values[index];
+  }
+
+  T& at(const std::size_t index) {
+    return m_values.at(index);
+  }
+
+  T pop_back() {
+    if (m_size == 0) {
+      throw std::out_of_range("Ring buffer is empty");
+    }
+    --m_size;
+    const auto last{m_values.back()};
+    m_values.pop_back();
+    return last;
+  }
+
+  bool empty() {
+    return m_size == 0U;
+  }
+
+  T pop_front() {
+    if (m_size == 0) {
+      throw std::out_of_range("Ring buffer is empty");
+    }
+    --m_size;
+    return m_values[m_start_index++];
+  }
+
+ private:
+  std::size_t m_size{};
+  std::vector<T> m_values{};
+  std::size_t m_start_index{};
+};
--- ring_buffer/ring_buffer_spec.cpp
+++ ring_buffer/ring_buffer_spec.cpp
@@ -2,6 +2,128 @@
 
 #include "ring_buffer.hpp"
 
-TEST(First, test) {
-  EXPECT_EQ(42, CalculateEverything());
+TEST(RingBuffer, check_capacity_of_ring_buffer_three) {
+  RingBuffer<int> unit{3};
+  EXPECT_EQ(3, unit.capacity());
+}
+
+TEST(RingBuffer, check_capacity_of_ring_buffer_five) {
+  RingBuffer<int> unit{5};
+  EXPECT_EQ(5, unit.capacity());
+}
+
+TEST(RingBuffer, check_size_of_empty_ring_buffer) {
+  RingBuffer<int> unit{3};
+  EXPECT_EQ(0, unit.size());
+}
+
+TEST(RingBuffer, add_one_element_into_ring_buffer) {
+  RingBuffer<int> unit{3};
+  unit.push_back(4);
+  EXPECT_EQ(1, unit.size());
+}
+
+TEST(RingBuffer, call_last_element) {
+  RingBuffer<int> unit{3};
+  unit.push_back(4);
+  EXPECT_EQ(4, unit.back());
+}
+
+TEST(RingBuffer, call_last_element_value_five) {
+  RingBuffer<int> unit{3};
+  unit.push_back(5);
+  EXPECT_EQ(5, unit.back());
+}
+
+TEST(RingBuffer, call_two_elements_of_ring_buffer) {
+  RingBuffer<int> unit{3};
+  unit.push_back(5);
+  unit.push_back(4);
+  EXPECT_EQ(5, unit[0]);
+  EXPECT_EQ(4, unit[1]);
+  EXPECT_EQ(2, unit.size());
+}
+
+TEST(RingBuffer, call_two_elements_of_ring_buffer_by_at) {
+  RingBuffer<int> unit{3};
+  unit.push_back(5);
+  unit.push_back(4);
+  EXPECT_EQ(5, unit.at(0));
+  EXPECT_EQ(4, unit.at(1));
+  EXPECT_EQ(2, unit.size());
+}
+
+TEST(RingBuffer, out_of_boundry_throws_exception) {
+  RingBuffer<int> unit{3};
+  unit.push_back(5);
+  EXPECT_THROW(unit.at(1), std::out_of_range);
+}
+
+TEST(RingBuffer, pop_back_returns_correct_value_and_reduces_size) {
+  RingBuffer<int> unit{3};
+  unit.push_back(5);
+  EXPECT_EQ(5, unit.pop_back());
+  EXPECT_EQ(0, unit.size());
+}
+
+TEST(RingBuffer, two_times_pop_back_returns_correct_value_and_reduces_size) {
+  RingBuffer<int> unit{3};
+  unit.push_back(5);
+  unit.push_back(4);
+  EXPECT_EQ(4, unit.pop_back());
+  EXPECT_EQ(1, unit.size());
+}
+
+TEST(RingBuffer, pop_back_throws_when_empty) {
+  RingBuffer<int> unit{3};
+  EXPECT_THROW(unit.pop_back(), std::out_of_range);
+}
+
+TEST(RingBuffer, pop_back_deletes_element) {
+  RingBuffer<int> unit{3};
+  unit.push_back(5);
+  unit.pop_back();
+  unit.push_back(4);
+  EXPECT_EQ(4, unit[0]);
+}
+
+TEST(RingBuffer, check_empty) {
+  RingBuffer<int> unit{3};
+  EXPECT_TRUE(unit.empty());
+  unit.push_back(3);
+  EXPECT_FALSE(unit.empty());
+}
+
+TEST(RingBuffer, pop_front) {
+  RingBuffer<int> unit{3};
+  unit.push_back(3);
+  EXPECT_EQ(3, unit.pop_front());
+  EXPECT_TRUE(unit.empty());
+}
+
+TEST(RingBuffer, pop_front_complex) {
+  RingBuffer<int> unit{3};
+  unit.push_back(3);
+  unit.push_back(4);
+  EXPECT_EQ(3, unit.pop_front());
+  EXPECT_EQ(4, unit.pop_front());
+}
+
+TEST(RingBuffer, pop_front_throws_when_empty) {
+  RingBuffer<int> unit{3};
+  EXPECT_THROW(unit.pop_front(), std::out_of_range);
+}
+
+TEST(RingBuffer, call_first_element_value_five) {
+  RingBuffer<int> unit{3};
+  unit.push_back(5);
+  EXPECT_EQ(5, unit.front());
+}
+
+TEST(RingBuffer, call_first_element_after_pop_front) {
+  RingBuffer<int> unit{3};
+  unit.push_back(5);
+  unit.push_back(4);
+  unit.pop_front();
+  EXPECT_EQ(4, unit.front());
 }
