8ebb5f115369958a97001c0e140483892ab70eb3
Johannes Mauthe <j.mauthe@samsung.com>
2020-05-06 17:54:54 +0200

    results after the session

--- ring_buffer/ring_buffer.hpp
+++ ring_buffer/ring_buffer.hpp
@@ -1,4 +1,73 @@
+#include <cstdio>
+#include <vector>
 
-int CalculateEverything(){
-    return 42;
-}
+template <typename T>
+class RingBuffer {
+ public:
+  explicit RingBuffer<T>(const std::size_t sz) : m_capacity(sz), m_size(0) {
+    m_buffer.resize(m_capacity);
+    m_tail = 0;
+    m_head = 0;
+  };
+
+  bool empty() {
+    return m_size == 0;
+  };
+
+  std::size_t size() {
+    return m_size;
+  }
+
+  void push_back(T elem) {
+    ++m_size;
+    if (m_size > static_cast<int>(m_capacity)) {
+      m_buffer[m_tail] = elem;
+      m_tail = (m_tail + 1) % m_capacity;
+      m_size = m_capacity;
+      m_head = (m_head + 1) % m_capacity;
+    } else {
+      m_buffer[m_tail] = elem;
+      m_tail++;
+    }
+  }
+
+  void push_front(T elem) {
+    if (m_head == 0) {
+      m_head = m_capacity - 1;
+    } else {
+      m_head--;
+    }
+    ++m_size;
+    m_buffer[m_head] = elem;
+  }
+
+  T pop_front() {
+    if (empty()) {
+      throw 1;
+    }
+    T elem = m_buffer[m_head];
+    m_head++;
+    --m_size;
+    return elem;
+  }
+
+  T pop_back() {
+    if (empty()) {
+      throw 1;
+    }
+    T elem = m_buffer[m_tail - 1];
+    m_tail--;
+    --m_size;
+    return elem;
+  }
+
+  T& operator[](const std::size_t index) {
+    return m_buffer[(m_head + index) % m_capacity];
+  }
+
+ private:
+  std::size_t m_capacity;  // capacity
+  int m_size;
+  std::vector<T> m_buffer;
+  std::size_t m_tail, m_head;
+};
--- ring_buffer/ring_buffer_spec.cpp
+++ ring_buffer/ring_buffer_spec.cpp
@@ -2,6 +2,64 @@
 
 #include "ring_buffer.hpp"
 
-TEST(First, test) {
-  EXPECT_EQ(42, CalculateEverything());
+class TestFixture : public ::testing::Test {
+ public:
+  RingBuffer<int> ring_buffer{5};
+};
+
+TEST_F(TestFixture, IsEmpty) {
+  EXPECT_TRUE(ring_buffer.empty());
 }
+
+TEST_F(TestFixture, Size) {
+  EXPECT_EQ(ring_buffer.size(), 0);
+}
+
+TEST_F(TestFixture, OneElement) {
+  ring_buffer.push_back(1);
+  EXPECT_FALSE(ring_buffer.empty());
+}
+
+TEST_F(TestFixture, PushedElementEqualsFirstElement) {
+  ring_buffer.push_back(4);
+  EXPECT_EQ(ring_buffer[0], 4);
+}
+
+TEST_F(TestFixture, PushOneElementInFront) {
+  ring_buffer.push_back(4);
+  ring_buffer.push_front(3);
+  EXPECT_EQ(ring_buffer[0], 3);
+}
+
+TEST_F(TestFixture, PopOneElementInFront) {
+  ring_buffer.push_back(4);
+  ring_buffer.push_front(3);
+  auto elem = ring_buffer.pop_front();
+  EXPECT_EQ(elem, 3);
+}
+
+TEST_F(TestFixture, PopOneElementAtBack) {
+  ring_buffer.push_back(4);
+  ring_buffer.push_front(3);
+  auto elem = ring_buffer.pop_back();
+  EXPECT_EQ(elem, 4);
+}
+
+TEST_F(TestFixture, PopOneElementInFrontFromEmpty) {
+  EXPECT_ANY_THROW(ring_buffer.pop_front());
+}
+
+TEST_F(TestFixture, PopOneElementBackFromEmpty) {
+  EXPECT_ANY_THROW(ring_buffer.pop_back());
+}
+
+TEST_F(TestFixture, CapacitySmallerThanSize) {
+  for(auto i=0;i<=5;++i)
+  {
+    ring_buffer.push_back(i);
+  }
+  EXPECT_EQ(ring_buffer[0],1);
+}
+
+
+
